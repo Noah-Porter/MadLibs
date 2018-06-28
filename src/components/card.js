5cfe3c6ff91179a18a404154ba344c9ebd92be86
Jared Grant <jared.grant@canyonsdistrict.org>
2018-06-28 11:33:47 -0600

    finish it

---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                this is where inputs belong.
+            </div>
+        )
+    }
+}
+
+export default Card;
---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <div className="input=>
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click the generate button to see the mad lib story</p>
+        </div>
+    )
+}
+
+export default Input
